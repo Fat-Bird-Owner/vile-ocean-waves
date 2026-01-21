278ca6e2df9c1c0fc0b87b76b49f674a9f2efe9b
Fat Bird’s Owner <lykongvibol2012@gmail.com>
2026-01-21 15:04:26 +0700

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -4,12 +4,7 @@ Planets.tantros.visible = true;
 Planets.tantros.accessible = true;
 Planets.tantros.alwaysUnlocked = true;
 
-Planets.tantros.ruleSetter = r -> {
-r.waveTeam = Team.blue;
-r.placeRangeCheck = true;
-r.showSpawns = true;
-r.coreDestroyClear = true;
-};
-
-
+var green = Team.get(4);
+green.name = "Basuals"
+  
 });
