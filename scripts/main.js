21e340fb8d78cc0b6d9b01e786a79ac5fbdeffa3
Fat Bird’s Owner <lykongvibol2012@gmail.com>
2026-01-23 13:53:21 +0700

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,13 @@
-/*
+
 Events.on(ContentInitEvent, e => { 
 // lol
 Planets.gier.visible = true;
 Planets.gier.accessible = true;
 
-var green = Team.green;
+var green = Team.get(4);
 
 });
-*/
+
 
 Events.on(SchematicCreateEvent, event => {
     Vars.ui.hudfrag.showToast("Schematic Saved.");
