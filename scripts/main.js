0127e5aaef3415f5a96ad7009efe80c87334cd53
Fat Bird’s Owner <lykongvibol2012@gmail.com>
2026-02-07 17:36:55 +0700

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -7,19 +7,10 @@ Planets.gier.accessible = true;
 // green.color = Color.valueOf("ffd37f");
 
 //quietUnlock
-    
+Blocks.disassembler:quietUnlock();
 });
 
 
 Events.on(SchematicCreateEvent, event => {
     Vars.ui.hudfrag.showToast("Schematic Saved.");
-})
-
-Events.on(PlayEvent, event => {
-
-Object.keys(Blocks).forEach(function (b) {
-if(Blocks[b] != null) {
-Blocks[b].quietUnlock();
-}})
-
 })
