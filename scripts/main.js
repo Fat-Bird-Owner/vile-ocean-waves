a67b5c92b2b72a10c4e3e7d83804fb61e162f154
Fat Bird’s Owner <lykongvibol2012@gmail.com>
2026-02-07 15:15:58 +0700

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -12,10 +12,6 @@ Events.on(SchematicCreateEvent, event => {
     Vars.ui.hudfrag.showToast("Schematic Saved.");
 })
 
-Events.on(Trigger.importMod, event => {
-    Vars.ui.hudfrag.showToast("Successfully imported.");
-})
-
-Events.on(Trigger.teamCoreDamage, event => {
-Vars.ui.hudfrag.showToast("[red]Core damaged.");
+Events.on(SectorLaunchEvent, event => {
+    event.sector.captureWave = 1;
 })
