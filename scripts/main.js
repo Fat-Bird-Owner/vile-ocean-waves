78e57b99c7515e726ad2290ba973b5180a833753
Fat Bird’s Owner <lykongvibol2012@gmail.com>
2026-02-08 12:01:22 +0700

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -18,5 +18,5 @@ Events.on(WorldLoadEvent, event => {
 
 Events.on(SectorLaunchEvent, event => {
     name = event.sector.preset.localizedName;
-    event.sector.setName("[salmon]Outpost - S:[]" + name)
+    event.sector.setName("[salmon]Outpost - S:[]" + name);
 })
