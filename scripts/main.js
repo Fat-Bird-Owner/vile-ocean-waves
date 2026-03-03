30485ee48c02f31270d0f883cd133bf9ec8a3b44
Fat Bird’s Owner <lykongvibol2012@gmail.com>
2026-03-03 18:11:55 +0700

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,56 +1,5 @@
 var lastUpdate = 0
 
-// Power button class
-
-Events.on(TapEvent, event => {
-    try {
-        
-        if(Vars.state.updateId == lastUpdate) return;
-
-        lastUpdate = Vars.state.updateId;
-        
-        const tile = event.tile;
-        if (!tile || !tile.build) return;
-
-        const target = Vars.content.getByName(ContentType.block, "siliconSmelter");
-        const effect = Fx.dooropenlarge
-
-        const block = tile.block();
-        if (block != target) return;
-
-        const build = tile.build;
-
-        //Vars.ui.showInfoToast(build.team + " " + event.player.team(),1);
-        
-        if (build.team != event.player.team()) return;
-        if (build.items) {
-
-        const sand = build.items.get(Items.sand);
-        const coal = build.items.get(Items.coal);
-
-        if (coal >= 1 && sand >= 2){
-
-        build.handleStack(Items.silicon,1,null);
-        build.removeStack(Items.sand,2);
-        build.removeStack(Items.coal,1);
-            
-        }
-            
-        }
-
-        if (effect) {
-            effect.at(build.x, build.y);
-        }
-
-        if (Sounds.click) {
-            Sounds.click.at(build.x, build.y);
-        }
-    } catch (e) {
-        // fails silently on iOS instead of crashing
-        Vars.ui.showInfoToast("error: " + e,1);
-    }
-});
-
 ////////
 Events.on(TapEvent, event => {
     try {
