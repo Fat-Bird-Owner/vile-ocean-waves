23b289cc7e9104fb8f23cabb0d5fa496f5b91043
Fat Bird’s Owner <lykongvibol2012@gmail.com>
2026-03-05 14:59:21 +0700

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -25,53 +25,6 @@ var target = Vars.content.getByName(ContentType.block, "neoplasiaReactor");
   }
 })
 
-// Power button class
-var lastUpdate = 0
-
-Events.on(TapEvent, event => {
-    try {
-        
-        if(Vars.state.updateId == lastUpdate) return;
-
-        lastUpdate = Vars.state.updateId;
-        
-        const tile = event.tile;
-        if (!tile || !tile.build) return;
-
-        const target = Vars.content.getByName(ContentType.block, "gr-button");
-        const effect = Vars.content.getByName(ContentType.block, "gr-button-tap").generateEffect;
-
-        const block = tile.block();
-        if (block != target) return;
-
-        const build = tile.build;
-
-        //Vars.ui.showInfoToast(build.team + " " + event.player.team(),1);
-        
-        if (build.team != event.player.team()) return;
-        if (build.power && build.power.graph) {
-            const pow = target.powerProduction * 60;
-            const graph = build.power.graph;
-            
-            graph.transferPower(pow);
-            graph.distributePower(0,30,true);
-        }
-
-        if (effect) {
-            effect.at(build.x, build.y);
-        }
-
-        if (Sounds.click) {
-            Sounds.click.at(build.x, build.y);
-        }
-    } catch (e) {
-        // fails silently on iOS instead of crashing
-        Vars.ui.showInfoToast("error: " + e,1);
-    }
-});
-
-
-
 ////////
 Events.on(TapEvent, event => {
     try {
