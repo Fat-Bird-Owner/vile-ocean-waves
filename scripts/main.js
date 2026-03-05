f2c10c2bd70422648c33cbb9961252907ccaf0b1
Fat Bird’s Owner <lykongvibol2012@gmail.com>
2026-03-05 18:33:24 +0700

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -2,26 +2,23 @@
 Events.on(GeneratorPressureExplodeEvent, event => {
 
 try {
-
 // Find entity otherwise return if is then if the block type isnt the target's also return
 
 var target = Vars.content.getByName(ContentType.block, "neoplasiaReactor");
   
-  if(event.build != null){  
-  if (event.build.block != target) return;
-
-    var x = event.build.tileX();
-    var y = event.build.tileY();
+  if(event.build && event.build.block == target){  
+  
+    var x = event.build.x;
+    var y = event.build.y;
     var block = Vars.content.getByName(ContentType.block, "gr-sporeoplasma");  
 
-    if (event.build.tile == null) return;
-    if (block == null) return;
+    if (!event.build.tile || !block) return;
 
     Vars.ui.announce("[red]///Sporeoplasma Detected///",4.5);
     
     Timer.schedule(() => {  
     event.build.tile.setBlock(block,Team.get(4),1);
-    }, 0.1);
+    }, 0.15);
 
   }
   
