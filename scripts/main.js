8cfedbe04a45fc3c4632d9a5669a932bf28c2ec4
Fat Bird’s Owner <lykongvibol2012@gmail.com>
2026-03-05 19:52:01 +0700

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -7,15 +7,15 @@ var string = event.message;
 const playerUnit = event.player.unit;
 var input = string.split(" ");
 
-if (input[1] === "!spawnU"){
+if (input[0] === "!spawn"){
 
-const unit = Vars.content.getByName(ContentType.unit, input[2]);
+const unit = Vars.content.getByName(ContentType.unit, input[1]);
 
 if (unit) {
-unit.spawn(player.team,player.x,player.y,player.rotation);
+unit.spawn(player.team(),player.x,player.y,player.rotation);
 
 } else {
-Vars.ui.showInfoToast("[red]" + input[2] + " Not found",2);
+Vars.ui.showInfoToast("[red]" + input[1] + " Not found",2);
 }
 
 ///
