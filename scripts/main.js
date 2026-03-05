517f82db91fd7a2c52a189ec5b2b5642d5a48536
Fat Bird’s Owner <lykongvibol2012@gmail.com>
2026-03-05 15:05:05 +0700

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -16,51 +16,12 @@ var target = Vars.content.getByName(ContentType.block, "neoplasiaReactor");
 
     if (event.build.tile == null) return;
     if (block == null) return;
+
+    Vars.ui.announce("[red]///Sporeoplasma Detected///",4.5);
     
     Timer.schedule(() => {  
     event.build.tile.setBlock(block,Team.get(4),1);
-    Vars.ui.announce("[red]///Sporeoplasma Detected///",4.5);
     }, 0.1);
 
   }
 })
-
-var lastUpdate = 0;
-
-////////
-Events.on(TapEvent, event => {
-    try {
-        
-        if(Vars.state.updateId == lastUpdate) return;
-
-        lastUpdate = Vars.state.updateId;
-        
-        const tile = event.tile;
-        if (!tile || !tile.build) return;
-
-        const target = Vars.content.getByName(ContentType.block, "copper-wall");
-
-        const block = tile.block();
-        if (block != target) return;
-
-        const build = tile.build;
-
-        Vars.ui.showInfoToast(build.team + " " + event.player.team(),1);
-        
-        if (build.team != event.player.team()) return;
-
-        Vars.ui.showInfoToast("[tan]Wave: " + Vars.state.wave, 1.5);
-        Vars.state.wavetime = 0;
-
-        if (Fx.generate && Fx) {
-            Fx.generate.at(build.x, build.y);
-        }
-
-        if (Sounds.click) {
-            Sounds.click.at(build.x, build.y);
-        }
-    } catch (e) {
-        // fails silently on iOS instead of crashing
-        Vars.ui.showInfoToast("error: " + e,1);
-    }
-});
