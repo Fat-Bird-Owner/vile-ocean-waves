77827e044614590711137ff52860fb1616a8d4d9
Fat Bird’s Owner <lykongvibol2012@gmail.com>
2026-03-06 10:04:26 +0700

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -17,12 +17,7 @@ const info = sector.info;
   var parent = TechTree.all.find(u => u.content == block);
 
 parent.each(n => {
-    if (n != parent){
-    n.content.clearUnlock();
-    n.finishedRequirements = ItemStack.empty;
-    }
-
-
+    n.reset();
 });
     
 }
