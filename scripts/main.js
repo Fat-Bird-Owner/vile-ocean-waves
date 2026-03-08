de50f1c13be0ca97ce31e72f82f832dd9e9d124b
Fat Bird’s Owner <lykongvibol2012@gmail.com>
2026-03-08 19:58:38 +0700

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -5,7 +5,7 @@ try{
 Vars.ui.showInfoToast("hit",1);
 
 const unit = e.unit;
-const damage = source.damage;
+const damage = bullet.damage;
 
 if (unit.type != target || damage <= 15 || unit.type.name != "gr-barracade") return;
 
