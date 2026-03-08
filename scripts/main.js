86babb80d5ad5fc3508b8e3f6a57d4c054486137
Fat Bird’s Owner <lykongvibol2012@gmail.com>
2026-03-08 21:07:33 +0700

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -6,11 +6,12 @@ const unit = e.unit;
 const damage = e.bullet.damage;
 const target = Vars.content.getByName(ContentType.unit, "gr-barracade");
 const maxThres = (unit.maxHealth / 10) * 2.5;
+const valid = unit.getDuration(StatusEffects.invincible);
 
-if (damage <= 15 || damage >= maxThres || unit.type != target) return;
+if (damage <= 15 || damage >= maxThres || unit.type != target || valid != 0) return;
     
 unit.apply(StatusEffects.invincible,9.5);
-Fx.magmasmoke.at(unit.x,unit.y);
+Fx.select.at(unit.x,unit.y);
 Sounds.unitCreateBig.at(unit.x,unit.y);
 
 } catch(e){
