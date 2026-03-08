96bd0145fa80bfe28dffbd2c9bc482a6f0353b3c
Fat Bird’s Owner <lykongvibol2012@gmail.com>
2026-03-08 22:17:54 +0700

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,17 +1,10 @@
 
-Events.on(UnitDamageEvent, e => {
+Events.on(UnitSpawnEvent, e => {
 try{
 const unit = e.unit;
-const damage = e.bullet.damage;
-const target = Vars.content.getByName(ContentType.unit, "gr-barracade");
-const maxThres = (unit.maxHealth / 10) * 2.5;
-const valid = unit.getDuration(StatusEffects.invincible);
-
-if (damage <= 15 || damage >= maxThres || unit.type != target || valid != 0) return;
-unit.apply(StatusEffects.invincible,9.5);
-Fx.select.at(unit.x,unit.y);
-Sounds.unitCreateBig.at(unit.x,unit.y);
-
+if (unit.type != Vars.content.unitType("gr-drive") || unit.type != Vars.content.unitType("gr-barracade")) return;
+unit.type.unlock();
+  
 } catch(e){
 Vars.ui.showInfoToast(e,3);
 }})
