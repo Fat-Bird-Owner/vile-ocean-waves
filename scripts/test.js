a6fb790b85ed8756c96219ea73e7de33f79bc2fe
Fat Bird’s Owner <lykongvibol2012@gmail.com>
2026-03-14 12:16:19 +0700

    Update test.js

--- scripts/test.js
+++ scripts/test.js
@@ -3,19 +3,6 @@ try{
 const myTurret = new ItemTurret("my-turret");
 myTurret.ammoTypes = Blocks.duo.ammoTypes;
 
-myTurret.buildType = () => {
-    const b = new JavaAdapter(ItemTurret.ItemTurretBuild, {
-
-        shoot(type){
-            this.super$shoot(type);
-            Vars.ui.showInfoToast("extra behavior", 2);
-        }
-
-    }, myTurret);
-
-    return b;
-};
-
 }catch(e){
     Vars.ui.showInfoToast(e, 10);
 }
