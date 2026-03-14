c1ced6890046e1f0bee229abeffdf14f43708fdb
Fat Bird’s Owner <lykongvibol2012@gmail.com>
2026-03-14 12:22:50 +0700

    Update test.js

--- scripts/test.js
+++ scripts/test.js
@@ -1,7 +1,37 @@
 try{
 
-const myTurret = new ItemTurret("my-turret");
-myTurret.ammoTypes = Blocks.duo.ammoTypes;
+function extendJS(){
+    const Base = arguments[0];
+    const def = arguments[arguments.length - 1];
+
+    const args = [Base, def];
+    for(let i = 1; i < arguments.length - 1; i++){
+        args.push(arguments[i]);
+    }
+
+    const instance = JavaAdapter.apply(null, args);
+
+    for(let k in def){
+        if(typeof def[k] !== "function"){
+            instance[k] = def[k];
+        }
+    }
+
+    return instance;
+}
+    // using your extend() polyfill
+
+
+const screamConveyor = extendJS(Conveyor, "scream-conveyor", {
+
+    placed(tile){
+        this.super$placed(tile);
+
+        Vars.ui.showInfoToast("AAAA", 2);
+    }
+
+});
+
 
 }catch(e){
     Vars.ui.showInfoToast(e, 10);
