bd6cf21233394cae0ad5e14a4b580515813bd8f7
Fat Bird’s Owner <lykongvibol2012@gmail.com>
2026-03-14 11:56:38 +0700

    Create test.js

---
+++ scripts/test.js
@@ -0,0 +1,8 @@
+const myTurret = new ItemTurret("my-turret");
+
+const oldShoot = myTurret.shoot;
+
+myTurret.shoot = function(tile){
+    oldShoot.call(this, tile);
+    Vars.ui.showInfoRoasr("extra behavior",2);
+};
