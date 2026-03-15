e1c6fbf5f8c2b387a863b6557f47d00177507441
Fat Bird’s Owner <lykongvibol2012@gmail.com>
2026-03-15 13:34:28 +0700

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -16,24 +16,6 @@ Vars.ui.showInfoToast(e,5);
 
 
 
-Events.on(UnitDamageEvent, e => {
-try{
-const unit = e.unit;
-const targetAsh = Vars.content.getByName(ContentType.unit, "gr-ash");
-const status = Vars.content.getByName(ContentType.status, "gr-rage");
-const maxThres = (unit.maxHealth / 2);
-const valid = unit.getDuration(status);
-  
-if (unit.health >= maxThres || valid != 0) return;
-if (unit.type != targetAsh) return;
-  
-unit.apply(status);
-
-} catch(e){
-Vars.ui.showInfoToast(e,3);
-}})
-
-
 
 /*
 Events.on(SectorLaunchEvent, event => {
