e30a793db576e6a95c5d9e8395aefe351343e285
Fat Bird’s Owner <lykongvibol2012@gmail.com>
2026-03-24 21:45:43 +0700

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,18 +1,3 @@
-Events.on(ContentInitEvent, event => {
-try {
-const target =  Blocks.additiveReconstructor;
-target.liquidCapacity = 100;
-    
-const cyan = target.consumeLiquid(Liquids.cyanogen,1/60);
-cyan.optional = true;
-
-} catch(e){
-Vars.ui.showInfoToast(e,3);
-    
-}
-  
-});
-
 
 Events.on(UnitCreateEvent, e => {
 try {
