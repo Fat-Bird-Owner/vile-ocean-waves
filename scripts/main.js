5e48d13373e257d2a3659da30ff4c9de1e1c13f7
Fat Bird’s Owner <lykongvibol2012@gmail.com>
2026-03-25 11:47:38 +0700

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -139,6 +139,35 @@ Vars.ui.showText("e",e,Align.center);
 },1.5);
 }});
 
+Events.on(GeneratorPressureExplodeEvent, event => {
+
+try {
+// Find entity otherwise return if is then if the block type isnt the target's also return
+
+var target = Vars.content.getByName(ContentType.block, "neoplasia-reactor");
+  
+  if(event.build && event.build.block == target){  
+  
+    var x = event.build.x;
+    var y = event.build.y;
+    var block = Vars.content.getByName(ContentType.block, "gr-sporeoplasma");  
+
+    if (!event.build.tile || !block) return;
+
+    Vars.ui.announce("[red]///Sporeoplasma Detected///",4.5);
+    
+    Timer.schedule(() => {  
+    event.build.tile.setBlock(block,Team.get(4),1);
+    }, 0.15);
+
+  }
+  
+} catch(e){
+Vars.ui.showInfoToast(e,3);
+}
+  
+})
+
 /*
 Events.on(UnitCreateEvent, e => {
 try{
