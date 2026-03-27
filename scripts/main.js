ab9712e19876da85de7844cf18ca4ee076d36256
Fat Bird’s Owner <lykongvibol2012@gmail.com>
2026-03-27 22:06:06 +0700

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -168,6 +168,32 @@ Vars.ui.showInfoToast(e,3);
   
 })
 
+
+Events.on(EventType.TapEvent, e => {
+try {
+const tile = e.tile;
+const block = tile.block();
+const target = Vars.content.getByName(ContentType.block, "gr-fabricator");
+
+if (block != target || tile.build == null) return;
+const sorted = tile.build.sorted;
+const build = tile.build;
+
+Sounds.click.at(build.x,build.y);
+    
+if (sorted == null || build == null) return;
+if(sorted instanceof UnitType) {
+sorted.spawn(tile.team(),build.x,build.y, rotation * 90);
+build.kill();
+
+} else if(sorted instanceof Block) {
+tile.setBlock(sorted,tile.team(),build.rotation);
+}
+    
+} catch(e){
+Vars.ui.showInfoToast(e,5);
+}});
+/*
 Events.on(EventType.TileChangeEvent, e => {
     try {
         if (!Vars.state.isGame() || Vars.state.isPaused()) return;
@@ -212,7 +238,7 @@ Events.on(EventType.TileChangeEvent, e => {
 });
 
 
-/*
+
 Events.on(UnitCreateEvent, e => {
 try{
 const spawner = e.spawner;
