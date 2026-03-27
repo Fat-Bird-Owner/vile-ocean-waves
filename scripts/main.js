71a66b903438298153663ec4477a456bbf282e6a
Fat Bird’s Owner <lykongvibol2012@gmail.com>
2026-03-27 08:29:18 +0700

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -174,7 +174,8 @@ try{
 let count = 0;
 let target = Vars.content.getByName(ContentType.block,"gr-core-gateway");
 let build = Vars.content.getByName(ContentType.block,"gr-core-gateway-build");
-
+let team = e.tile.team();
+    
 if (e.tile.block() != build) return;
     
 Groups.build.each(b => {
@@ -187,8 +188,8 @@ Vars.ui.showInfoToast("Core count: " + count,5);;
 if (count >= 2) return;
 
 Timer.schedule(() => {  
-e.tile.setBlock(target,e.tile.team(),1);
-}, 0.05);
+e.tile.setBlock(target,team,1);
+}, 0.01);
     
 } catch(e){
 Vars.ui.showInfoToast(e,5);
