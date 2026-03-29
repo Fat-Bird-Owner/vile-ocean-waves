8a1a2c25c40c6ae8f548fd37e03dcdbbe57eb30c
Fat Bird’s Owner <lykongvibol2012@gmail.com>
2026-03-29 19:30:31 +0700

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -212,7 +212,7 @@ const block = e.block();
 const target = Vars.content.getByName(ContentType.block,"command-block");
 const build = tile.build;
 
-if (block != target || build == null || build.team != player.team()) return
+if (block != target || build == null || build.team != player.team()) return;
 const team = build.team;
 Sounds.click.at(build.x,build.y);
 
@@ -232,7 +232,7 @@ Vars.ui.showInfoToast("Cleared Entities");
     
 });
 } catch(e){
-Vars.uishowInfoToast(e,5);
+Vars.ui.showInfoToast(e,5);
 }});
 
 
