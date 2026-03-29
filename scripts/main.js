c762f5b781c6cca3b3586d1d4c81ccb7a224f3b7
Fat Bird’s Owner <lykongvibol2012@gmail.com>
2026-03-29 19:35:59 +0700

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -225,12 +225,18 @@ Vars.ui.showMenu(
 ], 
 
 (i) => {
-if (i == 1){
+if (i == -1) return;
+if (i == 0){
 Groups.unit.clear();
-Vars.ui.showInfoToast("Cleared Entities");
+Vars.ui.showInfoToast("Cleared Entities", 2);
 }
-    
-});
+else if (i == 1){
+Vars.ui.showInfoToast("Test 1", 2);
+}
+else if (i == 2){
+Vars.ui.showInfoToast("Test 2", 2);
+}}
+
 } catch(e){
 Vars.ui.showInfoToast(e,5);
 }});
