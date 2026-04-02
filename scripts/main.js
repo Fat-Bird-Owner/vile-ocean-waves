5c36f87a8ace63bfec036cf0e70fca664d91da80
Fat Bird’s Owner <lykongvibol2012@gmail.com>
2026-04-02 22:23:55 +0700

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -170,7 +170,7 @@ Events.on(EventType.TapEvent, e => {
                     Vars.ui.showInfoToast(e,10);
                     }});
 
-                    Vars.ui.showText("[accent]Unit Entry[]",units,Align.center);
+                    Vars.ui.showText("[accent]Unit Entry[]",units.join(" "),Align.center);
                     
                     } catch(e){
                     Vars.ui.showInfoToast(e,10);
