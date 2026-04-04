9fcda1d41d0c5bb353ebaac40feddd17ff7a4f87
Fat Bird’s Owner <lykongvibol2012@gmail.com>
2026-04-04 22:28:15 +0700

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -11,14 +11,14 @@ const cont = Vars.ui.content.cont;
 if (!cont) return;
 
 cont.row();
-cont.add("[accent]Recipes[]").center().row();
+cont.add("[accent]Recipes[]").left().row();
 cont.add().height(3).row();
-cont.image().color(Pal.accent).height(3).width(400).row();
+cont.image().color(Pal.accent).height(3).width(400).left().row();
 
 cont.add().height(6).row();
-cont.add("[#8c7fa9]Lead[lightgrey] -> [#8c7fa9]Liquid Lead[]").center().row();
+cont.add("[#8c7fa9]Lead[lightgrey] -> [#8c7fa9]Liquid Lead[]").left().row();
 cont.add().height(6).row();
-cont.add("[#d99d73]Copper[lightgrey] -> [#d99d73]Liquid Copper[]").center().row();
+cont.add("[#d99d73]Copper[lightgrey] -> [#d99d73]Liquid Copper[]").left().row();
 
     
 } catch(e){
