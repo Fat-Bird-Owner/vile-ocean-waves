b8ac55d119c2616e7a9ccf2f3676e82c87e0213c
Fat Bird’s Owner <lykongvibol2012@gmail.com>
2026-04-09 16:50:50 +0700

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -24,8 +24,7 @@ const worldTile = Vars.world.tile(x,y).block();
 if (!worldTile) return;
     
 const attribute = worldTile.attributes.get(Attribute.get("beryllium"));
-building.potentialEfficiency = attribute;
-building.efficiency = attribute;
+building.potentialEfficiency = building.efficiency = building.optionalEfficiency = attribute;
 
 if (attribute <= 0){
 building.enabled = false;
