64ee29cfa762481b2c6efafb0b8fef9610a7593d
Fat Bird’s Owner <lykongvibol2012@gmail.com>
2026-04-23 22:47:47 +0700

    Update Idk.js

--- scripts/Idk.js
+++ scripts/Idk.js
@@ -1,12 +1,15 @@
 Events.run(Trigger.update, () => {
 try {
-if (Time.time % 20 > 1) return;
+if (Time.time % 60 > 1) return;
 if (!Weathers.snow.isActive()) return; 
   
 var builds = [];
 const block = Vars.content.block("gr-boiler");
   
 Groups.build.each(p => {
+
+if (p.block instanceof StackConveyor || p.block instanceof Conveyor || p.block instanceof duct) continue;
+  
 const next = Vars.indexer.findTile(
 p.team,
 p.x,
