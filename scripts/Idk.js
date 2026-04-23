276862081ace023d6c1f73e0eb707f1b29222ef1
Fat Bird’s Owner <lykongvibol2012@gmail.com>
2026-04-23 22:52:19 +0700

    Update Idk.js

--- scripts/Idk.js
+++ scripts/Idk.js
@@ -9,6 +9,7 @@ const block = Vars.content.block("gr-boiler");
 Groups.build.each(p => {
 
 if (p.block instanceof StackConveyor || p.block instanceof Conveyor || p.block instanceof Duct) return;
+if (p.block instanceof Conduit || p.block instanceof ItemBridge || p.block instanceof Router) return;
   
 const next = Vars.indexer.findTile(
 p.team,
