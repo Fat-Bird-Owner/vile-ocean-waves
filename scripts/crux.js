d7e460ddc6f97d0bc1736903d2ab514a375f669e
Fat Bird’s Owner <lykongvibol2012@gmail.com>
2026-04-24 22:00:09 +0700

    Update crux.js

--- scripts/crux.js
+++ scripts/crux.js
@@ -16,7 +16,7 @@ length: 0.1,
 lifetime: 600,
 colorTo: Color.valueOf("00000000"),
 colorFrom: Color.valueOf("2b2b2bff"),
-interp: Interp.sineIn
+interp: Interp.pow3In
 });
   
 particle.region = String(block.uiIcon);
@@ -24,6 +24,8 @@ particle.sizeFrom = particle.sizeTo = block.size * 3.95;
 particle.lifetime = Mathf.random(300,3000);
 particle.at(build.x, build.y);
 particle.baseLength = Mathf.random(-5,5);
+particle.length = Mathf.random(0.1,8);
+particle.spin = Mathf.random(-5, 5) / 10;
 particle.offset = Mathf.random(-15,15) + (build.rotation * 90);
   
 } catch(e){
