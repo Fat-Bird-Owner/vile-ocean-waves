5b0ef2090c6939b6d88ae2fc0ecf34f3b42527d6
Fat Bird’s Owner <lykongvibol2012@gmail.com>
2026-04-24 09:03:23 +0700

    Update crux.js

--- scripts/crux.js
+++ scripts/crux.js
@@ -1,22 +1,24 @@
-const particle = new ParticleEffect();
-Object.assign(particle, {
-particles: 1,
-cone: 15,
-cap: false,
-layer: 22,
-lifetime: 600,
-colorTo: Color.valueOf("00000000")
-});
-
 Events.on(BlockDestroyEvent, e => {
 try{ 
 const tile = e.tile;
 const block = tile.block();
 const build = tile.build;
 
+const particle = new ParticleEffect();
+Object.assign(particle, {
+particles: 1,
+cone: 15,
+cap: false,
+layer: 22,
+length: 0.1,
+lifetime: 600,
+colorTo: Color.valueOf("00000000")
+});
+  
 particle.region = String(block.uiIcon);
-particle.sizeFrom = particle.sizeTo = block.size * 8;
+particle.sizeFrom = particle.sizeTo = block.size * 4;
 particle.at(build.x, build.y);
+particle.offset = Mathf.random(-15,15);
   
 } catch(e){
 Vars.ui.showInfoToast(e,5); 
