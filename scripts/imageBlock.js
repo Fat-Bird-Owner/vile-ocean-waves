edf36e309330771d2ff6674bf05d64f1d0fa8823
Fat Bird’s Owner <lykongvibol2012@gmail.com>
2026-05-02 16:08:14 +0700

    Update imageBlock.js

--- scripts/imageBlock.js
+++ scripts/imageBlock.js
@@ -8,8 +8,8 @@ const {tile} = event;
 const tileBlock = tile.block();
 const {player} = event;
 
-if (!tile || !tile.build || !player || !player.team() || !player.team() != tile.team()) return;
-const image = atlas.fine(tile.build.string.toString());
+if (!tile || !tile.build || !player || !player.team() || !player.team() != tile.team() || player.selectedBlock != null) return;
+const image = atlas.find(tile.build.string.toString());
 const dialog = new BaseDialog("Info");
 
 if (image){
@@ -17,7 +17,10 @@ dialog.cont.add(image).grow();
 } else {
 dialog.cont.add(tile.build.string.toString()); 
 }
-  
+
+dialog.addCloseButton();
+dialog.show();
+
 } catch(e){
 Vars.ui.showInfoToast(e + "[red] - ImageBlock",5);
 }});
