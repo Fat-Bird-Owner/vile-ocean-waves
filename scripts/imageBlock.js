0ae4175b6c46c244de5b870eb3ad9e0b4547508c
Fat Bird’s Owner <lykongvibol2012@gmail.com>
2026-05-02 20:22:38 +0700

    Update imageBlock.js

--- scripts/imageBlock.js
+++ scripts/imageBlock.js
@@ -17,11 +17,11 @@ if (slice.length >= 1){
 const sentence = slice.slice(1).join(" ");
 }
   
-const image = atlas.find(tile.build.message.toString());
+const contImage = atlas.find(tile.build.message.toString());
 const dialog = new BaseDialog("Info");
   
 if(image && image.found()){
-dialog.cont.add(new Image(image)).size(image.width * 1.25, image.height * 1.25);
+dialog.cont.add(new Image(contImage)).size(image.width * 1.25, image.height * 1.25);
 } else {
 dialog.cont.add(tile.build.message.toString()); 
 }
