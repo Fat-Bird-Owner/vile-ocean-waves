47ebaaf0921311b8fd8e493a19dea60b673f7e0c
Fat Bird’s Owner <lykongvibol2012@gmail.com>
2026-05-02 16:09:00 +0700

    Fix logical error in tile and player team check

--- scripts/imageBlock.js
+++ scripts/imageBlock.js
@@ -8,7 +8,7 @@ const {tile} = event;
 const tileBlock = tile.block();
 const {player} = event;
 
-if (!tile || !tile.build || !player || !player.team() || !player.team() != tile.team() || player.selectedBlock != null) return;
+if (!tile || !tile.build || !player || !player.team() || player.team() != tile.team() || player.selectedBlock != null) return;
 const image = atlas.find(tile.build.string.toString());
 const dialog = new BaseDialog("Info");
 
