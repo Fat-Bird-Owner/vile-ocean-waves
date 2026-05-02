3197a2453e4bf2e4bca9531ad220f18637b71df9
Fat Bird’s Owner <lykongvibol2012@gmail.com>
2026-05-02 20:32:41 +0700

    Update imageBlock.js

--- scripts/imageBlock.js
+++ scripts/imageBlock.js
@@ -2,39 +2,46 @@ const {content} = Vars;
 const {atlas} = Core;
 const block = content.block("message");
 
-Events.on(TapEvent,event => {
+Events.on(TapEvent, event => {
 try {
-const {tile} = event;
-const tileBlock = tile.block();
-const {player} = event;
 
-if (!tile || !tile.build || !player || !player.team() || player.team() != tile.team() || player.selectedBlock != null) return;
-const message = tile.build.message.toString();
-const slice = message.split(" ");
-const image = slice[0];
-let sentence;
+    const {tile, player} = event;
 
-if (slice.length >= 1){
-sentence = slice.slice(1).join(" ");
-}
-  
-const contImage = atlas.find(image);
-const dialog = new BaseDialog("Info");
-  
-if(contImage && contImage.found()){
-dialog.cont.add(new Image(contImage)).size(image.width * 1.25, image.height * 1.25);
-} else {
-dialog.cont.add(image); 
-}
+    if(!tile || !tile.build || !player) return;
+    if(player.team() != tile.team()) return;
+    if(player.selectedBlock != null) return;
+    if(tile.build.block != block) return;
 
-if (slice.length >= 1){
-dialog.cont.row();
-dialog.cont.add(sentence);
-}
+    const message = String(tile.build.message || "").trim();
+    if(message.length <= 0) return;
 
-dialog.addCloseButton();
-dialog.show();
+    const slice = message.split(" ");
+    const imageName = slice[0];
+    let sentence = "";
+
+    if(slice.length > 1){
+        sentence = slice.slice(1).join(" ");
+    }
+
+    const contImage = atlas.find(imageName);
+    const dialog = new BaseDialog("Info");
+
+    if(contImage && contImage.found()){
+        dialog.cont.image(contImage)
+            .size(contImage.width * 1.25, contImage.height * 1.25);
+    }else{
+        dialog.cont.add(imageName);
+    }
+
+    if(sentence.length > 0){
+        dialog.cont.row();
+        dialog.cont.add(sentence).wrap().center()
+    }
+
+    dialog.addCloseButton();
+    dialog.show();
 
 } catch(e){
-Vars.ui.showInfoToast(e + "[red] - ImageBlock",5);
-}});
+    Vars.ui.showInfoToast(e + "[red] - ImageBlock", 5);
+}
+});
