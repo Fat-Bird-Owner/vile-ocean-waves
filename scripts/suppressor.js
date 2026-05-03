ffef687b6ce4c0be3eefe71d2a110f4c23534910
Fat Bird’s Owner <lykongvibol2012@gmail.com>
2026-05-03 22:20:00 +0700

    Update suppressor.js

--- scripts/suppressor.js
+++ scripts/suppressor.js
@@ -7,7 +7,10 @@ Events.on(BlockDestroyEvent, event => {
 try {
 const {tile} = event;
 let found = false;
+  
 const targetFloor = Vars.content.block("meld-meld-crystal-floor");
+const transitFloor = Vars.content.block("meld-meld-melting-floor");
+const finalFloor = Vars.content.block("meld-meld-swampland");
   
 for (let i = 0; i < blocks.length; i++){
 if (tile.block() == Vars.content.block(blocks[i]) found = true;
@@ -22,8 +25,14 @@ if (!t || !t.floor() || t.floor() != targetFloor) return;
 
 Time.runTask(dur, () => {
 dur += Mathf.random(0.15, 0.35);
-t.setFloor(Vars.content.block("meld-meld-melting-floor");
+t.setFloor(transitFloor);
 Fx.vapor.at(t.worldx(), t.worldy(), Color.valueOf(Vars.content.item("meld-meld").color));
+
+Time.runTask(1.22, () => {
+t.setFloor(finalFloor);
+Fx.vapor.at(t.worldx(), t.worldy(), Color.valueOf(Vars.content.item("meld-meld").color));
+});
+  
 });
 
 } catch(e){
