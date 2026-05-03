a0d529746abfefc0fcc2ba3996e764afeee53b20
Fat Bird’s Owner <lykongvibol2012@gmail.com>
2026-05-03 22:16:05 +0700

    Update suppressor.js

--- scripts/suppressor.js
+++ scripts/suppressor.js
@@ -7,10 +7,21 @@ Events.on(BlockDestroyEvent, event => {
 try {
 const {tile} = event;
 let found = false;
-
+const targetFloor = Vars.content.block("meld-meld-crystal-floor");
+  
 for (let i = 0; i < blocks.length; i++){
 if (tile.block() == Vars.content.block(blocks[i]) found = true;
 }
+
+if (!found) return;
+tile.circle(range, t => {
+try {
+if (!t || !t.floor() || t.floor() != targetFloor) return;
+
+
+} catch(e){
+Vars.ui.showInfoToast(e,5);
+}});
 
 } catch(e){
 Vars.ui.showInfoToast(e,5);
