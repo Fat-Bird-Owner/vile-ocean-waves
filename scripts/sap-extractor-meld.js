f214c9ffc7e659613fe1e2e978f780548bcb41f8
Fat Bird’s Owner <lykongvibol2012@gmail.com>
2026-05-03 13:51:13 +0700

    Update sap-extractor-meld.js

--- scripts/sap-extractor-meld.js
+++ scripts/sap-extractor-meld.js
@@ -21,8 +21,19 @@ const {build} = tile;
 
 if (!tile || !build || build.block != block || !build.liquids) return;
 
-if (build.front().block == block2){
+let bx = build.x;
+let by = build.y;
+if (build.rotation == 0) bx++;
+else if (build.rotation == 1) by++;
+else if (build.rotation == 2) bx--;
+else by--;
+
+const blockWall = Vars.world.tileWorld(bx, by).block();
+
+if (blockWall == block2){
 build.liquids.set(liquid, build.block.liquidCapacity);
+}
+
 }
   
 } catch(e){
