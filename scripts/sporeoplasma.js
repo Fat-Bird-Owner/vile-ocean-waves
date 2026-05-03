4d4bac5d30033cfdce14e4a6933d0ed72a78fdec
Fat Bird’s Owner <lykongvibol2012@gmail.com>
2026-05-03 19:58:29 +0700

    Correct loop and enhance error handling
    Fix loop condition and improve error handling in sporeoplasma.js.

--- scripts/sporeoplasma.js
+++ scripts/sporeoplasma.js
@@ -6,13 +6,15 @@ const tileBlock = tile.block();
 
 if (!block || !tile || tileBlock != block) return;
 for (let i = 0; i < 0; i++){
+Time.runTask(0.45,() => {
+try{
+  
 const ro = Mathf.round(Mathf.random(0,3));
 const spreadTile = tile.nearby(ro);
   
 if(!spreadTile) continue;
 if (!spreadTile.solid() && spreadTile.block() != block){
-Time.runTask(0.45,() => {
-try{
+  
 spreadTile.setBlock(block, tile.team());
 } catch(e){
 Vars.ui.showInfoToast(e,5);
