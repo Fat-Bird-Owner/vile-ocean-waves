168b2b7e0d2c419f98b51fe1c1b5f7099d547ec1
Fat Bird’s Owner <lykongvibol2012@gmail.com>
2026-05-03 11:15:54 +0700

    Update structureBlock.js

--- scripts/structureBlock.js
+++ scripts/structureBlock.js
@@ -16,12 +16,11 @@ if (build.block == content.block(blocks[i])) found = true;
 }
   
 if (!found || !build || !source || !lightBlock || !targets) return;
-const lightBlockWrap = lightBlock.wrap(Color.valueOf(build.block.baseColor));
 
 targets.each(b => {
 try {
 b.heal(damage * 0.4);
-lightBlockWrap.at(b.x, b.y, b.block.size);
+lightBlock.at(b.x, b.y, b.block.size, Color.valueOf(build.block.baseColor));
 
 } catch(e){
 Vars.ui.showInfoToast(e + "[red] - StructureBlock Inner", 5);
