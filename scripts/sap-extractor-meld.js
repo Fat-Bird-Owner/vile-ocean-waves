8412109610fb471ca8f7215fdd99a168102b7195
Fat Bird’s Owner <lykongvibol2012@gmail.com>
2026-05-03 13:36:29 +0700

    Update sap-extractor-meld.js

--- scripts/sap-extractor-meld.js
+++ scripts/sap-extractor-meld.js
@@ -1,5 +1,24 @@
 const block = Vars.content.block("meld-sap-extractor");
-const block2 = Vars.content.block("meld-earthen-wall-fissure"); 
 
-block.stats.replace(Stat.tiles, StatValues.content(block2));
+const block2 = Vars.content.block("meld-earthen-wall-fissure"); 
+const block3 = Vars.content.block("meld-floor-nectar"); 
+
+const liquid = Vars.content.liquid("meld-valid");
+
+const seq = new Seq();
+seq.addAll([block2, block3]);
+
+block.stats.replace(Stat.tiles, StatValues.content(seq));
 block.stats.remove(Stat.input);
+
+Events.on(TileChangeEvent, e => {
+try {
+const {tile} = e;
+const {build} = tile;
+
+if (!tile || !build || tile.block() != block) return;
+build.liquid.set(liquid, tile.block.liquidCapacity);
+
+} catch(e){
+Vaes.ui.showInfoToast(e,5); 
+}});
