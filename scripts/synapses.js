560a76ec2bbe237cc593373be0f13867a5fdcdbc
Fat Bird’s Owner <lykongvibol2012@gmail.com>
2026-05-03 19:25:19 +0700

    Update synapses.js

--- scripts/synapses.js
+++ scripts/synapses.js
@@ -5,6 +5,25 @@ Events.on(BlockDestroyEvent, event => {
 try {
 const {tile} = event;
 const blockTile = tile.block();
+if (blockTile != block) return;
+const {build} = tile;
+let delay = 0;
+
+indexer.eachBlock(
+build.team, 
+build.x, 
+build.y, 
+8 * Vars.tileSize 
+b => b.block() != block,
+b => {
+delay += Mathf.random(0.05, 0.1);
+Time.runTask(delay, {
+b.kill();
+});
+
+}  
+);
+
   
 } catch(e){
 Vars.ui.showInfoToast(e,5);
