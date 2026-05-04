1bdea5bd276e1120a1aec5c702f8a815f33ad009
Fat Bird’s Owner <lykongvibol2012@gmail.com>
2026-05-04 17:25:13 +0700

    Update Logic wire.js

--- scripts/Logic wire.js
+++ scripts/Logic wire.js
@@ -7,13 +7,6 @@ Events.on(TapEvent, event => {
         if(!tile.build || tile.block() != targetBlock) return;
 
         function nearby(build){
-            const newTile = build.tile;
-            Fx.generate.at(newTile.build.x, newTile.build.y);
-            
-            const nextTile = newTile.nearby(build.rotation);
-            if(nextTile == null) return;
-            
-            nearby(nextTile.build);
         }
 
         Fx.generate.at(tile.build.x, tile.build.y);
