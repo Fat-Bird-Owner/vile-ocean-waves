bbb94af61a014260dc0ca6a8d28ac160faa989da
Fat Bird’s Owner <lykongvibol2012@gmail.com>
2026-05-04 22:16:49 +0700

    Update Logic wire.js

--- scripts/Logic wire.js
+++ scripts/Logic wire.js
@@ -15,7 +15,7 @@ Events.on(TapEvent, event => {
         if (!frontBuild || !build || frontBuild.block != wireBlock) return;
         const {block} = frontBuild;
         const circuitRate = block.attributes.get(Attribute.get("circuitRate"));
-        const circuitHeatingDamage = block.attributes.get(Attribute.get("circuitHeatingDamage"));
+        const circuitHeatingDamage = block.attributes.get(Attribute.get("circuitHeatDamage"));
             
         Fx.dooropen.at(frontBuild.x, frontBuild.y, frontBuild.block.size);
         for (let i = 0; i < heating.length; i++){
