61a0e81fe7eb25a5eeac54b56832ea84d82f28fb
Fat Bird’s Owner <lykongvibol2012@gmail.com>
2026-05-06 21:56:22 +0700

    Update sporeoplasma.js

--- scripts/sporeoplasma.js
+++ scripts/sporeoplasma.js
@@ -3,7 +3,7 @@ try {
 const {tile} = event;
 const block = Vars.content.block("gr-sporeoplasma");
 const tileBlock = tile.block();
-const limit = 275;
+const limit = 165;
   
 if (!block || !tile || tileBlock != block) return;
 
@@ -15,9 +15,13 @@ for (let i = 0; i < 3; i++){
   
 const ro = Mathf.round(Mathf.random(0,3));
 const spreadTile = tile.nearby(ro);
-output(Vars.state.stats.placedBlockCount.get(block, 0));
+
+let size = 0;
+Groups.build.each(b => {
+if (b.block == block) size++;
+});
   
-if(!spreadTile || Vars.state.stats.placedBlockCount.get(block, 0) >= limit) continue;
+if(!spreadTile || size >= limit) continue;
 if (!spreadTile.solid() && spreadTile.block() != block){
 spreadTile.setBlock(block, tile.team());
 }
