6fd667b1670a16519a3e04dffaadd6d6bc430b05
Fat Bird’s Owner <lykongvibol2012@gmail.com>
2026-05-08 18:37:17 +0700

    Update CrackedScepter.js

--- scripts/CrackedScepter.js
+++ scripts/CrackedScepter.js
@@ -7,13 +7,14 @@ const weapon1 = unit.weapons.get(1);
 weapon1.reload = 8;
 weapon1.shootStatus = StatusEffects.slow;
 weapon1.shootStatusDuration = 2.25 * 60;
-weapon1.bullet.inaccuracy = 15;
+weapon1.bullet.inaccuracy = 20;
 
 const shoot = new ShootPattern(); 
-shoot.shots = 15;
+shoot.shots = 4;
 shoot.shotDelay = 0;
 
 for (let i = 2; i < unit.weapons.size; i++){
 const smol = unit.weapons.get(i);
 smol.shoot = shoot;
+smol.inaccuracy = 25;
 }
