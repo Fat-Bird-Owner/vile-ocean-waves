331f9d0e3e83d4f5b6a2c039ef4ac6c7f6d0f642
Fat Bird’s Owner <lykongvibol2012@gmail.com>
2026-05-17 22:24:39 +0700

    Update Idk.js

--- scripts/Idk.js
+++ scripts/Idk.js
@@ -4,43 +4,55 @@ let time = 0;
 
 Events.run(Trigger.update, () => {
 try {
-if (Time.time % 60 > 1) return;
-if (!Weathers.snow.isActive()) return; 
-  
+
+if (!Weathers.snow.isActive()) return;
+
 const block = Vars.content.block("gr-boiler");
 
 time += Time.delta;
-if (build == null || time >= 60){
-builds = Groups.build;
+
+if (time < 60) return;
+
+builds = Groups.build.copy();
 time = 0;
-}
-  
+
+if (builds == null) return;
+
 builds.each(p => {
 try {
 
 if (!p || !p.isValid() || !p.block.update) return;
-if (p.block instanceof StackConveyor || p.block instanceof Conveyor || p.block instanceof Duct) return;
-if (p.block instanceof Conduit || p.block instanceof ItemBridge || p.block instanceof Router) return;
-  
+
+if (
+p.block instanceof StackConveyor ||
+p.block instanceof Conveyor ||
+p.block instanceof Duct ||
+p.block instanceof Conduit ||
+p.block instanceof ItemBridge ||
+p.block instanceof Router
+) return;
+
 const next = Vars.indexer.findTile(
 p.team,
 p.x,
-p.y, 
+p.y,
 10 * Vars.tilesize,
 b => b.block == block
 );
-  
-if (!next && p.block != block) {
+
+if (!next && p.block != block && p.status() == BlockStatus.active) {
 p.enabled = true;
-p.applySlowdown(0.5 , 60);
+p.applySlowdown(0.5, 60);
 p.damage(p.maxHealth / 20);
 fx.at(p.x, p.y);
 }
 
 } catch(e){
-Vars.ui.showInfoToast(e + "[red] - inner",5);
-}});
+Vars.ui.showInfoToast(String(e) + "[red] - inner", 5);
+}
+});
 
 } catch(e){
-Vars.ui.showInfoToast(e + " - Outer" ,5);
-}});
+Vars.ui.showInfoToast(String(e) + " - Outer", 5);
+}
+});
